7542c6e4dda20f7d7312545f1899ce8ad643a3bb
al-shahzeb <99374818+al-shahzeb@users.noreply.github.com>
2023-06-11 18:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+var team = players;
+var team1 = [];
+players.forEach((player) => {
+	team1.push(player);
+})
+
+var cap1 = person;
